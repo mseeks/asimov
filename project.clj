30d9508232ef329b82d42ceeffb902043fbd2278
Matthew Sullivan <msull92@gmail.com>
2013-01-30 20:59:22 -0600

    add clutch

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject asimov "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+  				 [com.ashafa/clutch "0.4.0-RC1"]
+				 [environ "0.3.0"]])
--- src/asimov/core.clj
+++ src/asimov/core.clj
@@ -1,1 +1,7 @@
-(ns asimov.core)
+(ns asimov.core
+	(use [[com.ashafa.clutch]
+		  [environ.core]]))
+
+(def db (assoc (cemerick.url/url "https://msull92.cloudant.com" "asimov")
+                    :username (env :asimov-db-username)
+                    :password (env :asimov-db-password)))
