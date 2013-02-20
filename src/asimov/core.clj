554e3408a68d7abd91fcbd3a57357c6aee4e235c
Matthew Sullivan <msull92@gmail.com>
2013-02-20 17:41:16 -0600

    Majore changes

--- src/asimov/core.clj
+++ src/asimov/core.clj
@@ -2,10 +2,19 @@
 	(:require [clojure.string :as string])
 	(:require [cheshire.core :as json])) ; should probably use clojure.data.json instead, it's more of a standard
 
-(defn parse [text]
-	(string/split text #"\W+")) ; find each and every word ... like sherlock holmes
+(defn words_in [string]
+	(string/split string #"\W+")) ; find each and every word ... like sherlock holmes
 
 (defn relate [text] ; relate the words with its attributes and to other words eventually
 	(json/generate-string
-		(for [word (parse text)] {word {:length (count word)}}))) ; creates json like the one below
+		(for [word (words_in text)] {word {:length (count word)}}))) ; creates json like the one below
 		; [{"hello": {"length": 5}},{"world": {"length": 5}}]
+
+(defn sort-by-frequency ; temporary
+	"Sorts words by frequency."
+	{:added "0.0.1"}
+	[string]
+	(reverse
+		(sort-by second 
+			(frequencies
+				(words_in string)))))
--- test/asimov/test/core.clj
+++ test/asimov/test/core.clj
@@ -2,9 +2,11 @@
 	(:use [asimov.core])
 	(:use [clojure.test]))
   
-(deftest parsing
-	(is (= ["hello" "world"] (parse "hello world"))))
+(deftest words
+	(is (= ["hello" "world"] (words_in "hello world"))))
 
 (deftest relating
 	(println (relate "hello world!")))
 	;(is (= "{\"words\"{\"hello\"{\"length\":5}}}" (relate "hello"))))
+	
+(deftest sorting (println (sort-by-frequency (slurp "/Users/msull92/Projects/asimov/demo.txt"))))
